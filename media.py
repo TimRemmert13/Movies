e725c77513934ff7f75e80a22ef51b0302cdef6c
timremmert <tbr23@scarletmail.rutgers.edu>
2018-05-11 14:35:00 -0400

    initial commit of project

---
+++ media.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+class Movie (): 
+    
+    def __init__(self, title, storyline, image, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
+
+    def show_trailer(self): 
+        webbrowser.open(self.trailer_youtube_url)
+
